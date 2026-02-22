ffea209e6a5b4f32714afb5178d408c2989b99f6
Stexinator <hurricane90@gmx.de>
2026-02-22 10:47:32 +0100

    update master to release

--- util/build.mjs
+++ util/build.mjs
@@ -1,49 +1,11 @@
 import { compilePack } from "@foundryvtt/foundryvtt-cli";
-import { existsSync } from "fs";
 import fs from "fs/promises";
-import path from "path";
 
-// Clean output directory, or create build directory
-const outDir = path.resolve(process.cwd(), "build");
-if (existsSync(outDir)) {
-    const filesToClean = (await fs.readdir(outDir)).map((dirName) => path.resolve(outDir, dirName));
-    for (const file of filesToClean) {
-        await fs.rm(file, { recursive: true });
-    }
-} else {
-    await fs.mkdir(outDir);
-}
+const MODULE_ID = process.cwd();
 
-console.log("Clean Finished");
-
-// Build packs
-const packFolders = await fs.readdir("packs");
-for (const pack of packFolders) {
-    await compilePack(`packs/${pack}`, path.resolve(outDir, `packs/${pack}`), { recursive: true });
-}
-
-console.log("Build Packs Finished");
-
-// Copy files and folders to output
-const files = ["module.json"];
-for (const file of files) {
-    await fs.cp(file, path.resolve(outDir, file), { recursive: true });
-}
-console.log("Build Complete");
-
-if (process.argv[2] === "--watch") {
-    const watcher = fs.watch(process.cwd(), { recursive: true });
-    console.log("Watching Files");
-    for await (const event of watcher) {
-        const file = event.filename.split(path.sep)[0];
-        if (files.includes(file)) {
-            const outFile = path.resolve(outDir, file);
-            if (existsSync(file)) {
-                await fs.cp(file, outFile , { recursive: true });
-            } else {
-                await fs.rm(outFile, { recursive: true })
-            }
-            console.log("Files updated");
-        }
-    }
+const packs = await fs.readdir('./jsonData');
+for (const pack of packs) {
+    if (pack === '.gitattributes') continue;
+    console.log('Packing ' + pack);
+    await compilePack(`${MODULE_ID}/jsonData/${pack}`, `${MODULE_ID}/wtrpg-compendium/packs/${pack}`, { recursive: true });
 }
